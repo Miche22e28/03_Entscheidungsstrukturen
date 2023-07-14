9f3a6177d79a2ff1f3d88a1aa3b163a5f9694212
miche22e28 <mbergamasco11@googlemail.com>
2023-07-14 10:53:34 +0200

    IF

--- script_01.js
+++ script_01.js
@@ -3,8 +3,8 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
-const ageJohn = 20;
-const ageMark = 30;
+const ageJohn = 30;
+const ageMark = 20;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -19,3 +19,21 @@ console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------");
+
+/************ IF ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// Fake it, `till you make it!
+// if(isJohnOlder)
+if(iageJohn > ageMark)
+{
+    console.log("John ist älter.");
+}
+
+/************ IF - ELSE ************/
+
+// mit Alternative
+
+// entweder ja oder nein
