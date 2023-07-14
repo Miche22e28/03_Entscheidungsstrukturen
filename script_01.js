52edd566056dc2d55d1351b5b55b51895e0179cc
miche22e28 <mbergamasco11@googlemail.com>
2023-07-14 09:11:23 +0200

    Ausgabe

--- script_01.js
+++ script_01.js
@@ -3,3 +3,15 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+const ageJohn = 20;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
